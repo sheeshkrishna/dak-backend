70a4071e747c65fa11f84e3e86fa82bd75b8eb81
Krishnasheesh Datta <126602314+sheeshkrishna@users.noreply.github.com>
2025-09-18 13:05:23 +0530

    Update models.py

--- models.py
+++ models.py
@@ -6,11 +6,11 @@ class User(SQLModel, table=True):
     username: str = Field(index=True, unique=True)
     hashed_password: str
     is_active: bool = Field(default=True)
-    role: str = Field(default="user")
+    role: str = Field(default="user")  # roles could be "admin" or "user"
 
 class DAK(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     title: str
     description: str
-    status: str = Field(default="open")
+    status: str = Field(default="open")  # open, forwarded, closed
     created_by: int
